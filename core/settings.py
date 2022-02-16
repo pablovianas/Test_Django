37116f286b97daf071a187f81f94162ddb76d7ff
Pablo Viana <96837989+pablovianas@users.noreply.github.com>
2022-02-16 14:04:42 -0300

    Updated settings

--- core/settings.py
+++ core/settings.py
@@ -132,8 +132,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "templates/static"),)
-STATIC_ROOT = BASE_DIR / "staticfiles"
+
+if DEBUG:
+    STATICFILES_DIRS = (os.path.join(BASE_DIR, "templates/static"),)
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Configure Django App for Heroku.
 django_heroku.settings(locals())
