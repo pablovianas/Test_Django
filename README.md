5e5213ff8b9e0c828d4bfe2a5c1bc1d132b11986
Pablo Viana <96837989+pablovianas@users.noreply.github.com>
2022-02-16 10:51:15 -0300

    Application deploy

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# Test_Django
-Django test app for a job application
+# Test Django
+
+### Installing 
+
+#### Clone 
+`git clone https://github.com/pablovianas/Test_Django`
+
+#### Dependencies 
+` pip install -r requirements.txt`
+
+#### Migrate database
+`python manage.py migrate`
+
+#### Start server
+
+`python manage.py runserver`
--- core/settings.py
+++ core/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 from decouple import config
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -71,6 +72,9 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'core.wsgi.application'
+
+# Configure Django App for Heroku.
+django_heroku.settings(locals())
 
 
 # Database
